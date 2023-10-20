da95c5ea568876b3aa8204264cd7c9d2749a3ef1
nowgamesyt <75790722+nowgamesyt@users.noreply.github.com>
2023-10-20 03:07:03 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 const ENDPOINT = 'https://tiktok-tts.weilnet.workers.dev'
 
-const TEXT_BYTE_LIMIT = 300
+const TEXT_BYTE_LIMIT = 5000
 const textEncoder = new TextEncoder()
 
 window.onload = () => {
